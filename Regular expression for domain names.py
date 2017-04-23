2c14b9d07c3c3907857116fbf7dd917486ce6d0d
vallisu <syslik110@gmail.com>
2017-04-23 13:24:33 +0300

    Correct

--- Regular expression for domain names.py
+++ Regular expression for domain names.py
@@ -3,7 +3,7 @@
 import re
 
 name = input("Please enter a domain name: ")
-result = re.match(r"^(http[s]?://)([a-zA-Z0-9](([.\-]?[a-zA-Z0-9]?){0,61}[a-zA-Z0-9])?)\.([a-z]{1,6})(\/)?$", name)
+result = re.match(r"^(http[s]?://)([a-zA-Z0-9](([.\-]?[a-zA-Z0-9]){0,61}[a-zA-Z0-9])?)\.([a-z]{1,6})(\/)?$", name)
 if result:
     print("match")
 else:
